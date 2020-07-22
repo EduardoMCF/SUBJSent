56b36a5a36099b217d6226099b84c7839fd5ff48
Eduardo C <eduardo.freitas@ccc.ufcg.edu.br>
2020-07-22 18:37:46 -0300

    Resolvido erro ao salvar resultados do modelo

--- subj_sent/utils/util.py
+++ subj_sent/utils/util.py
@@ -1,24 +1,28 @@
 import matplotlib.pyplot as plt
 
 import os
+from re import search
 from pathlib import Path
 
-def log(msg : str):
+
+def log(msg: str):
     def log_decorator(func):
-        def wrapper(*args,**kwargs):
-            print(f"{msg}...", end = " ")
-            result = func(*args,**kwargs)
+        def wrapper(*args, **kwargs):
+            print(f"{msg}...", end=" ")
+            result = func(*args, **kwargs)
             print('finished')
             return result
         return wrapper
     return log_decorator
 
-def plot_history(history : object, path : str):
+
+def plot_history(history: object, path: str):
     metrics_dict = history.history
-    metrics_names = list(filter(lambda s: not s.startswith('val'), metrics_dict.keys()))
+    metrics_names = list(
+        filter(lambda s: not s.startswith('val'), metrics_dict.keys()))
 
     fig, ax = plt.subplots(nrows=len(metrics_names), ncols=1)
-    for i,metric in enumerate(metrics_names):
+    for i, metric in enumerate(metrics_names):
         ax[i].plot(metrics_dict[metric])
         ax[i].plot(metrics_dict[f"val_{metric}"])
         ax[i].set_title(f'model {metric}')
@@ -26,13 +30,17 @@ def plot_history(history : object, path : str):
         ax[i].set_xlabel('epoch')
         ax[i].legend(['train', 'validation'], loc='upper left')
 
-    fig.tight_layout()    
+    fig.tight_layout()
     fig.savefig(f"{path}.png")
-    
-def savefile_name(path : str, folderName = "Model") -> str:
+
+
+def savefile_name(path: str, folderName="Model") -> str:
     path = Path(__file__).parent.parent.parent / path
-    files = os.listdir(path)
-    count = max(map(lambda name: int(name.split(' ')[-1]),files)) + 1 if files else 1
+    pattern = '^%s [0-9]{1,}$' % folderName
+    folders = list(filter(lambda elem: bool(
+        search(pattern, elem)), os.listdir(path)))
+    count = max(map(lambda elem: int(
+        elem.split()[-1]), folders))+1 if folders else 1
     path = path / f"{folderName} {count}"
     os.mkdir(path)
-    return path,count
+    return path, count
